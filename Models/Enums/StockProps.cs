44f38c009738c5814027603f3151ceab3fd71413
KiraiChang <kirai.chang@gmail.com>
2020-03-07 00:26:39 +0800

    FirstVersion

---
+++ Models/Enums/StockProps.cs
@@ -0,0 +1,51 @@
+namespace FinanceApi.Models.Enums {
+    /// <summary>
+    /// StockProps Index
+    /// </summary>
+    public enum StockProps {
+        /// <summary>
+        /// 日期
+        /// </summary>
+        Date = 0,
+
+        /// <summary>
+        /// 成交股數
+        /// </summary>
+        Volume = 1,
+
+        /// <summary>
+        /// 成交金額
+        /// </summary>
+        Amount = 2,
+
+        /// <summary>
+        /// 開盤價
+        /// </summary>
+        OpenPrice = 3,
+
+        /// <summary>
+        /// 最高價
+        /// </summary>
+        MaxPrice = 4,
+
+        /// <summary>
+        /// 最低價
+        /// </summary>
+        MinPrice = 5,
+
+        /// <summary>
+        /// 收盤價
+        /// </summary>
+        ClosePrice = 6,
+
+        /// <summary>
+        /// 漲跌價差
+        /// </summary>
+        Decline = 7,
+
+        /// <summary>
+        /// 成交筆數
+        /// </summary>
+        Count = 8,
+    }
+}
---
+++ Models/Stocks.cs
@@ -0,0 +1,65 @@
+using System;
+
+namespace FinanceApi.Models
+{
+    /// <summary>
+    /// 股票的Model
+    /// </summary>
+    public class Stocks
+    {
+        /// <summary>
+        /// 流水號
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// 股票Id
+        /// </summary>
+        public string StockId { get; set; }
+
+        /// <summary>
+        /// 日期
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// 開盤價
+        /// </summary>
+        public decimal OpenPrice { get; set; }
+        
+        /// <summary>
+        /// 最高價
+        /// </summary>
+        public decimal MaxPrice { get; set; }
+
+        /// <summary>
+        /// 最低價
+        /// </summary>
+        public decimal MinPrice { get; set; }
+
+        /// <summary>
+        /// 收盤價
+        /// </summary>
+        public decimal ClosePrice { get; set; }
+
+        /// <summary>
+        /// 漲跌價差
+        /// </summary>
+        public decimal Decline  { get; set; }
+        
+        /// <summary>
+        /// 成交股數
+        /// </summary>
+        public long Volume { get; set; }
+
+        /// <summary>
+        /// 成交金額
+        /// </summary>
+        public long Amount  { get; set; }
+
+        /// <summary>
+        /// 成交筆數
+        /// </summary>
+        public long Count  { get; set; }
+    }
+}
