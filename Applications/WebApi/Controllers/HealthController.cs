ae3bbd44024e4d9ff8e5e9bf308bdad5fae27d90
kirai <kirai.chang@ark88.net>
2026-05-20 15:00:33 +0800

    add for check controller

---
+++ Applications/WebApi/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    /// <summary>
+    /// check health controller
+    /// </summary>
+    public class HealthController : Controller
+    {
+        /// <summary>
+        /// check health for system
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("health")]
+        public string Index()
+        {
+            return "OK!";
+        }
+    }
+}
